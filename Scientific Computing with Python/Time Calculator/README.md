885a8cb52db10e277c7635dd6df3b0fb9514613f
maschub <marvin.schubert@gmx.net>
2023-05-09 14:54:44 +0200

    Time Calculator completed

--- Scientific Computing with Python/Time Calculator/README.md
+++ Scientific Computing with Python/Time Calculator/README.md
@@ -3,7 +3,7 @@ Write a function named add_time that takes in two required parameters and one op
 
 - a start time in the 12-hour clock format (ending in AM or PM)
 - a duration time that indicates the number of hours and minutes
-- (optional) a starting day of the week, case insensitive
+- (optional) a starting day of the week, case-insensitive
 - The function should add the duration time to the start time and return the result.
 
 If the result will be the next day, it should show (next day) after the time. If the result will be more than one day later, it should show (n days later) after the time, where "n" is the number of days later.
--- Scientific Computing with Python/Time Calculator/time_calculator.py
+++ Scientific Computing with Python/Time Calculator/time_calculator.py
@@ -1,7 +1,85 @@
-def add_time(start, duration):
+def add_time(start, duration, starting_day=""):
+    # error handling will not be performed only on start variable
+    # project reads "Assume that the start time are valid times"
 
+    try:
+        duration_hours, duration_minutes = [int(x) for x in duration.split(':')]
+        if any([
+            duration_hours < 0,
+            duration_minutes >= 60
+        ]):
+            raise
+    except:
+        return "Error: Please check your time to add."
 
+    weekday_mapper = {
+        'monday': 0,
+        'tuesday': 1,
+        'wednesday': 2,
+        'thursday': 3,
+        'friday': 4,
+        'saturday': 5,
+        'sunday': 6
+    }
 
+    if starting_day and starting_day.lower() not in weekday_mapper:
+        return "Error: You entered an unknown weekday."
 
+    start_hour, start_minute = start.split(':')
+    start_minute, start_segment = start_minute.split()
+    start_hour = int(start_hour)
+    start_minute = int(start_minute)
+
+    day_counter = 0
+    start_minute = duration_minutes + start_minute
+    if start_minute >= 60:
+        start_hour += 1
+        start_minute -= 60
+        if start_hour == 12:
+            start_segment, daychanged = toggle_twelve_hour(start_segment)
+            if daychanged:
+                day_counter += 1
+
+    while duration_hours > 0:
+        start_hour += 1
+        duration_hours -= 1
+
+        if start_hour == 12:
+            start_segment, daychanged = toggle_twelve_hour(start_segment)
+            if daychanged:
+                day_counter += 1
+
+        if start_hour == 13:
+            start_hour -= 12
+
+    if day_counter == 1:
+        dayhint = " (next day)"
+    elif day_counter > 1:
+        dayhint = f" ({day_counter} days later)"
+    else:
+        dayhint = ""
+
+    if starting_day:
+        weekday_index = weekday_mapper[starting_day.lower()]
+        while day_counter > 0:
+            weekday_index += 1
+            day_counter -= 1
+        new_weekday = f", {str([*weekday_mapper][weekday_index%7]).capitalize()}"
+    else:
+        new_weekday = ""
+
+    new_time = f'{start_hour}:{start_minute:02d} {start_segment}{new_weekday}{dayhint}'
 
     return new_time
+
+def toggle_twelve_hour(segment):
+    daychanged = False
+    if segment == "PM":
+        segment = "AM"
+        daychanged = True
+    else:
+        segment = "PM"
+    return (segment, daychanged)
+
+if __name__ == '__main__':
+    print(add_time("6:30 PM", "205:12"))
